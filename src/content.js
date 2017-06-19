063ab25ac744c7a789d9089d4ce10327636a870c
ann <anna@synqq.com>
2017-06-19 22:36:05 +0500

    Add manifest file with the description of the extension, add scripts for main extension button and script for working with DOM on the real page

---
+++ src/content.js
@@ -0,0 +1,31 @@
+chrome.runtime.onMessage.addListener(function (request) {
+    if (!request) return;
+
+    if (request.command === 'FormatText') {
+        formatReleaseNotes();
+    }
+});
+
+function formatReleaseNotes() {
+    var $releaseNotesTextArea = $('#editcopy'),
+        releaseNotesText;
+
+    if ($releaseNotesTextArea.length > 0) {
+        releaseNotesText = $releaseNotesTextArea.val();
+        $releaseNotesTextArea.val(removeUnneededSymbols(releaseNotesText));
+    } else {
+        alert('Release Notes haven\'t been found');
+    }
+}
+
+function removeUnneededSymbols(text) {
+    return text.replace(/<li>\[<a href='/gi, '')
+               .replace(/<\/li>/gi, '')
+               .replace(/'>.*?<\/a>\]/gi, '')
+               .replace(/<h2>/gi, '')
+               .replace(/<\/h2>/gi, '')
+               .replace(/<ul>/gi, '')
+               .replace(/<\/ul>/gi, '')
+               .replace(/  /gi, '')
+               .replace(/\n\n/gi, '\n');
+}
